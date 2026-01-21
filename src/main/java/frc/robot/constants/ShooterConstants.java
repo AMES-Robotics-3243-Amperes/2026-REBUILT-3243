de58e7c8e870997017337c8120093fc051d3a4bd
Oliver <127167853+KrumpToast@users.noreply.github.com>
2026-01-21 05:16:04 -0700

    added double shooter files and created constants

---
+++ src/main/java/frc/robot/constants/ShooterConstants.java
@@ -0,0 +1,20 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.constants;
+
+/** Add your docs here. */
+public class ShooterConstants {
+
+    public static final int shooterLeaderId = 3;
+    public static final int shooterFollowerId = 4;
+
+    public static final int hoodPivotId = 5;
+
+    public static final double shooterGearRatio = 1;
+    public static final double shooterMaxOutput = 1;
+
+    public static final double hoodPivotGearRatio = 5;
+    public static final double hoodPivotMaxOutput = 1;
+}

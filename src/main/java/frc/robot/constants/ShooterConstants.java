16d86f45c596a938c43f7f2b828b7b0390101f8e
bryce <utbryceh@gmail.com>
2026-03-13 22:01:05 -0600

    Update at 'Fri Mar 13 22:01:05 MDT 2026'

--- src/main/java/frc/robot/constants/ShooterConstants.java
+++ src/main/java/frc/robot/constants/ShooterConstants.java
@@ -65,7 +65,7 @@ public class ShooterConstants {
   // hood
   public static final int hoodId = 5;
 
-  public static final Angle hoodAbsoluteEncoderZeroedRotation = Degrees.of(17.9);
+  public static final Angle hoodAbsoluteEncoderZeroedRotation = Degrees.of(19.2);
   public static final Angle hoodMinRotation = Degrees.of(17);
   public static final Angle hoodMaxRotation = Degrees.of(46);
 
